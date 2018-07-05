c73fd0693b0a9d21cd029f7dc4880bdebc4d2351
nandan145 <40564246+nandan145@users.noreply.github.com>
2018-07-05 19:03:55 +0530

    Add files via upload

---
+++ Azurelogin.ps1
@@ -0,0 +1,4 @@
+$azureAccountName ="cab5b932-2b76-4d97-bcc5-208eba2359b6"
+$azurePassword = ConvertTo-SecureString "xC+iDTZ+iBxFisXpHU5nDpmLgX1PTmHlbYtDOdP6PG8=" -AsPlainText -Force $psCred = New-Object System.Management.Automation.PSCredential($azureAccountName, $azurePassword) #Login-AzureRmAccount -C -Credential $psCred
+
+Add-AzureRmAccount -Credential $psCred -TenantId d790955c-7903-4754-842b-32503d9b6edb -ServicePrincipal
